f38d5c5bb8fc87192d1fa8c78291cb56e50f931a
LiangWangLab <71713540+LiangWangLab@users.noreply.github.com>
2021-01-11 21:47:25 +0700

    Create CpG_annotation_coverage.sh

--- CpG_annotation_coverage.sh
+++ CpG_annotation_coverage.sh
@@ -1,18 +1,12 @@
 #Create CpG annotations reference
 Rscript CpG_annotations_reference.R
 
-#Create a folder named 'data' for output
-import os
-path = os.getcwd()
-for i in range(0,40):
-    file_path = path + '\data'+ str(i)
-    os.mkdir(file_path)
-
 #Loop through all bam files for CpG annotations and output in 'data'
-for i in $(ls *.bam | cut -c 1-2)
+mkdir -p data
+for i in $(ls *.bam)
 do   
        echo $i CpGI coverage 
-       bedtools coverage -a hg19_CpG_annotation.bed -b $i.rmdup.bam -counts > $i.txt
+       bedtools coverage -a hg19_CpG_annotation.bed -b $i.bam -counts > data/$i.txt
 done
 
 #Generate TPM normalized summary table
