c0f750bc077dba8bb2b6c209a9025619170dca9f
LiangWangLab <71713540+LiangWangLab@users.noreply.github.com>
2021-01-11 21:29:21 +0700

    Create CpG_annotation_coverage.sh

--- CpG_annotation_coverage.sh
+++ CpG_annotation_coverage.sh
@@ -8,7 +8,7 @@ for i in range(0,40):
     file_path = path + '\data'+ str(i)
     os.mkdir(file_path)
 
-#Loop through all bam files for CpG annotations
+#Loop through all bam files for CpG annotations and output in 'data'
 for i in $(ls *.bam | cut -c 1-2)
 do   
        echo $i CpGI coverage 
