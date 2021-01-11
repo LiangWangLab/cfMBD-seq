cf687255824e82a6439def44abcce937b4f4ad8e
LiangWangLab <71713540+LiangWangLab@users.noreply.github.com>
2021-01-11 21:48:26 +0700

    Create CpG_annotation_coverage.sh

--- CpG_annotation_coverage.sh
+++ CpG_annotation_coverage.sh
@@ -5,7 +5,7 @@ Rscript CpG_annotations_reference.R
 mkdir -p data
 for i in $(ls *.bam)
 do   
-       echo $i CpGI coverage 
+       echo $i CpG annotations
        bedtools coverage -a hg19_CpG_annotation.bed -b $i.bam -counts > data/$i.txt
 done
 
