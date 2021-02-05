84136b4d9cda30343b0aab816e10ef57e952d1be
LiangWangLab <71713540+LiangWangLab@users.noreply.github.com>
2021-02-05 10:40:19 -0500

    Update CpG_annotation_coverage.sh

--- CpG_annotation_coverage.sh
+++ CpG_annotation_coverage.sh
@@ -3,7 +3,7 @@ Rscript CpG_annotations_reference.R
 
 #Loop through all bam files for CpG annotations and output in 'data'
 mkdir -p data
-for i in $(ls *.bam)
+for i in $(ls *.bam | cut -c 1-2)
 do   
        echo $i CpG annotations
        bedtools coverage -a hg19_CpG_annotation.bed -b $i.bam -counts > data/$i.txt
